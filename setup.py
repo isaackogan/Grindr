f6fb83b4748f4c66538c74d77ef1b3d2592a287e
isaackogan <isaacikogan@gmail.com>
2024-12-29 01:13:24 -0500

    Set your active profile photos

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ manifest: dict = {
     "name": "Grindr",
     "license": "MIT",
     "author": "Isaac Kogan",
-    "version": "0.1.8.post3",
+    "version": "0.1.9",
     "email": "info@isaackogan.com"
 }
 
